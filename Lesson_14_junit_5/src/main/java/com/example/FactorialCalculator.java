2b78e9000979741313ff0fef238e40d3305542c1
Paruyr <paruyr@example.com>
2025-05-02 22:33:40 +0300

    Lesson_7 new

--- Lesson_14_junit_5/src/main/java/com/example/FactorialCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class FactorialCalculator {
-    public long calculateFactorial(int n) {
-        if (n < 0) throw new IllegalArgumentException("Число не может быть отрицательным");
-        return n == 0 ? 1 : n * calculateFactorial(n - 1);
-    }
-}
--- Lesson_14_junit_5/src/main/java/com/example/TriangleArea.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class TriangleArea {
-    public double calculateArea(double base, double height) {
-        if (base <= 0 || height <= 0) throw new IllegalArgumentException("Основание и высота должны быть положительными");
-        return (base * height) / 2;
-    }
-}
